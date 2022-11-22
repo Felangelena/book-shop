94ef7b0b35e37922b0c7e03ac5e9aa37899c17f2
Olena Oliianenko <felangelena@gmail.com>
2022-11-22 01:53:43 +0100

    feat: add popups

--- script.js
+++ script.js
@@ -1,31 +1,110 @@
-fetch('books.json') //path to the file with json data
-.then((response) => response.json())
-.then((data) => {
-    document.body.onload = addElements(data);
-    console.log(data);
+window.addEventListener('DOMContentLoaded', function () {
+    fetch('books.json') //path to the file with json data
+    .then((response) => response.json())
+    .then((data) => {
+        document.body.onload = addElements(data);
+        console.log(data);
+    });
+    //console.log(books[1].author);
+
+    function addElements(data) {
+        document.body.innerHTML = '<main></main>';
+        const box = document.querySelector('main');
+
+        //CartBtn
+        const cartBtn = document.createElement("button");
+        cartBtn.textContent = 'Bag';
+        cartBtn.classList.add("cartBtn"),
+        box.appendChild(cartBtn);
+
+        // title Book Catalog
+        const title = document.createElement("h1");
+        title.textContent = 'Book Catalog';
+        box.appendChild(title);
+
+        // Grid
+        const grid = document.createElement("div");
+        grid.classList.add('grid');
+        box.appendChild(grid);
+
+        // Grid items
+        for (var i = 0; i < data.length; i++) {
+            grid.innerHTML += `<div class='grid-item'><img src="${data[i].imageLink}" alt="${data[i].title}"><div class="book-title">${data[i].title}</div><div class="book-author">${data[i].author}</div><div class="book-price">Price: $${data[i].price}</div><div class="book-btn"><a class="btn show-more">Show more</a><a class="btn add">Add to bag</a></div></div>`;
+        }
+
+        //Cart
+        function createCart() {
+            const cart = document.createElement("div"),
+            cardItem = document.createElement("div"),
+            closeBtn = document.createElement("button"),
+            buyBtn = document.createElement("button");
+            cart.classList.add('cart'),
+            cardItem.classList.add("cart-item"),
+            closeBtn.classList.add("btn-close", "btn");
+            buyBtn.classList.add("btn-buy", "btn");
+            closeBtn.textContent = 'CLOSE';
+            buyBtn.textContent = 'BUY';
+            document.body.appendChild(cart);
+            cart.appendChild(cardItem);
+            cardItem.appendChild(closeBtn);
+            cardItem.appendChild(buyBtn);
+        }
+        createCart();
+
+        let cart = document.querySelector('.cart'),
+            closeBtn = document.querySelector('.btn-close');
+
+        function openCart() {
+            cart.style.display = 'block';
+        }
+        function closeCart() {
+            cart.style.display = 'none';
+        }
+
+        cartBtn.addEventListener('click', openCart);
+        closeBtn.addEventListener('click', closeCart);
+
+        let btnShowMore = document.querySelectorAll('.show-more'),
+            btnAdd = document.querySelectorAll('.add'),
+            item = document.querySelectorAll('.grid-item');
+
+        //Popup
+        const bcg = document.createElement("div"),
+            popup = document.createElement("div"),
+            close = document.createElement("button"),
+            popupDesc = document.createElement("div");
+        bcg.classList.add('popup-bcg'),
+        popup.classList.add("popup-container"),
+        close.classList.add("popup-close", "btn");
+        popupDesc.classList.add("popup-desc"),
+        close.textContent = 'Close';
+        document.body.appendChild(bcg);
+        bcg.appendChild(popup);
+        popup.appendChild(close);
+        popup.appendChild(popupDesc);
+
+        function openPopup(i) {
+            bcg.style.display = 'block';
+            popupDesc.textContent = `${data[i].description}`;
+        }
+        function closePopup() {
+            bcg.style.display = 'none';
+        }
+
+        close.addEventListener('click', closePopup);
+
+        btnShowMore.forEach((item, i) => {
+            item.addEventListener('click', () => {
+                openPopup(i);
+            })
+        });
+    };
 });
-//console.log(books[1].author);
 
-function addElements(data) {
-    document.body.innerHTML = '<main></main>';
 
-    // title Book Catalog
-    const title = document.createElement("h1");
-    title.textContent = 'Book Catalog';
-    const box = document.getElementsByTagName('main')[0];
-    box.appendChild(title);
 
-    // Grid
-    const grid = document.createElement("div");
-    grid.classList.add('grid');
-    box.appendChild(grid);
 
-    // Grid items
-    for (var i = 0; i < data.length; i++) {
-        grid.innerHTML += `<div class='grid-item'><img src="${data[i].imageLink}" alt="${data[i].title}"><div class="book-title">${data[i].title}</div><div class="book-author">${data[i].author}</div><div class="book-price">Price: $${data[i].price}</div><div class="book-btn"><a class="btn show-more">Show more</a><a class="btn add">Add to bag</a></div></div>`;
-    }
 
-}
 /*     const gridItem = document.createElement("div");
     gridItem.classList.add('grid-item', 'text-lg'); */
 
